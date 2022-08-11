2427dfb67eb12548e9587dad17ca95a97563953f
Jason M Cobb <Jason_M_Cobb@carmax.com>
2022-08-11 07:02:01 -0400

    added problems

---
+++ src/CodingProblemsTests/HackerRankTests/FormingMagicSquareTests.cs
@@ -0,0 +1,13 @@
+namespace CodingProblemsTests.HackerRankTests;
+
+public class FormingMagicSquareTests
+{
+    [Theory]
+    [InlineData(1, 2, 3, "Cat B")] // Default test in problem
+    [InlineData(1, 3, 2, "Mouse C")] // Default test in problem
+    public void ElectronicsShopReturnsCorrectOutput(int catA, int catB, int mouseC, string expectedOutput)
+    {
+        var result = CatsAndAMouse.CatAndMouse(catA, catB, mouseC);
+        Assert.Equal(expectedOutput, result);
+    }
+}
